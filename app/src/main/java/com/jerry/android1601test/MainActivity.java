1fd9719c972adef490f2515e3b05802e2785a78d
mac <cecszj@163.com>
2016-04-28 23:04:17 +0800

    修改内容

--- app/src/main/java/com/jerry/android1601test/MainActivity.java
+++ app/src/main/java/com/jerry/android1601test/MainActivity.java
@@ -2,6 +2,7 @@ package com.jerry.android1601test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv;
     }
 }
